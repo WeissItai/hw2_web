d4d189be812a3e08dedbf1f5fee29868f9be6496
Ron <ronzadi@gmail.com>
2021-05-18 16:18:12 +0300

    build index

---
+++ create_index.py
@@ -0,0 +1,102 @@
+
+import os
+import json
+import pprint
+from math import log, sqrt
+import xml.etree.ElementTree as ET
+from process_language import clear_text
+from collections import defaultdict, Counter
+
+
+# Extract relevant text for each document in given file.
+# Maps doc_id to text
+def parse_xml(fname):
+
+    data = {}
+
+    tree = ET.parse(fname)
+    root = tree.getroot()
+    for record in root.findall("./RECORD"):
+        doc_id = int(record.find("./RECORDNUM").text)
+
+        # add title
+        title = record.find("./TITLE").text
+        data[doc_id] = clear_text(title)
+
+        # add extract
+        extract = getattr(record.find("./ABSTRACT"), 'text', '')
+        data[doc_id] += '\n' + clear_text(extract)
+
+        # add abstract
+        abstract = getattr(record.find("./EXTRACT"), 'text', '')
+        data[doc_id] += '\n' + clear_text(abstract)
+    #
+    # p = pprint.PrettyPrinter()
+    # p.pprint(data)
+    return data
+
+
+# Create index and write to json file
+def create_corpus(dir_path):
+
+    corpus = {}
+
+    for filename in os.listdir(dir_path):
+        if filename.endswith(".xml"):
+            data = parse_xml(os.path.join(dir_path, filename))
+            corpus.update(data)
+
+    return corpus
+
+
+def build_index(dir_path):
+
+    tfidf = defaultdict(dict)
+    idf = defaultdict(lambda: 0.0)
+    lengths = defaultdict(lambda: 0.0)
+
+    num_docs = 0
+
+    c = Counter()
+    corpus = create_corpus(dir_path)
+
+    for doc_id, text in corpus.items():
+
+        # Compute TF
+        c.clear()
+        c.update(text.split())
+        for term, cnt in c.items():
+            tfidf[term][doc_id] = cnt
+            idf[term] += 1
+
+        num_docs += 1
+
+    # Compute IDF
+    for term, docs in tfidf.items():
+        idf_ = log(num_docs / idf[term])
+        for doc_id in list(docs.keys()):
+            docs[doc_id] = docs[doc_id] * idf_
+
+    # Compute vector lengths
+    for token, scores in tfidf.items():
+        for doc_id, score in scores.items():
+            lengths[doc_id] += score ** 2
+
+    for doc_id in lengths.keys():
+        lengths[doc_id] = sqrt(lengths[doc_id])
+
+
+    p = pprint.PrettyPrinter()
+    p.pprint(lengths)
+
+    data = {
+        'vec_len': lengths,
+        'tf_idf': tfidf
+    }
+
+    with open('vsm_inverted_index.json', 'w') as f:
+        json.dump(data, f)
+
+
+if __name__ == "__main__":
+    build_index("C:\Dev\hw2_web\cfc-xml_corrected")
---
+++ process_language.py
@@ -0,0 +1,42 @@
+
+from string import punctuation
+
+stopwords = {
+    'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours',
+    'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers',
+    'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves',
+    'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are',
+    'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does',
+    'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until',
+    'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into',
+    'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down',
+    'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here',
+    'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more',
+    'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so',
+    'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now'
+             }
+
+
+# Remove punctuation
+def clear_word(s):
+    for c in punctuation:
+        if c in s:
+            s = s.replace(c, ' ')
+    return s.strip()
+
+
+def clear_text(txt):
+    cleaned = ""
+    for word in txt.split():
+        word = clear_word(word)
+        if word in stopwords:
+            continue
+        cleaned += " " + word
+
+    return cleaned
+
+
+
+
+
+
